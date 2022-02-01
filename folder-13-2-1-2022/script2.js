90a9f2a4d3ee0445e4dc5808965f4519885a5d57
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-02-02 00:00:39 +0330

    added files

--- folder-13-2-1-2022/script2.js
+++ folder-13-2-1-2022/script2.js
@@ -1,5 +1,105 @@
-const boxDiv = document.getElementById("box");
+// const boxDiv = document.getElementById("box");
 
-boxDiv.addEventListener("click", (e) => {
-  alert("Hello, Mohammadhossein!");
-});
+// boxDiv.addEventListener("click", (e) => {
+//   console.log(e);
+// //   alert("Hello, Mohammadhossein!");
+// });
+
+// const numOne = document.getElementById("num-one");
+// const numTwo = document.getElementById("num-two");
+// const addSum = document.getElementById("add-sum");
+
+/*
+ # Javascrip Events:
+  * click
+  * mouseenter
+  * mouseleave
+  * mousedown
+  * mouseup
+  * mousemove
+  * keydown
+  * keyup
+  * blur
+  * focus
+ */
+
+// const add = () => {
+//   let one = parseFloat(numOne.value) || 0;
+//   let two = parseFloat(numTwo.value) || 0;
+//   addSum.innerHTML = `Your sum is: ${one + two}`;
+// };
+// numOne.addEventListener("input", add);
+// numTwo.addEventListener("input", add);
+
+// numOne.addEventListener("click", () => {
+//   alert("Hi!");
+// });
+
+// numOne.addEventListener("mouseenter", () => {
+//   alert("Hi!");
+// });
+
+// numOne.addEventListener("mouseleave", () => {
+//   alert("Hi!");
+// });
+
+// numOne.addEventListener("mousedown", () => {
+//   alert("Hi!");
+// });
+
+// numOne.addEventListener("mouseup", () => {
+//   alert("Hi!");
+// });
+
+// numOne.addEventListener("mousemove", () => {
+//   alert("Hi!");
+// });
+
+// numOne.addEventListener("keydown", () => {
+//   alert("Hi!");
+// });
+
+// numOne.addEventListener("keyup", () => {
+//   alert("Hi!");
+// });
+
+// numOne.addEventListener("blur", () => {
+//   alert("Hi!");
+// });
+
+// numOne.addEventListener("focus", () => {
+//   alert("Hi!");
+// });
+
+// const dog = document.getElementById("dog");
+// const dogPic = document.getElementById("dogPic");
+
+// dog.addEventListener("click", () => {
+//   if (dogPic.className === "hide") {
+//     dogPic.className = "";
+//   } else {
+//     dogPic.className = "hide";
+//   }
+// });
+
+// const cat = document.getElementById("cat");
+// const catPic = document.getElementById("catPic");
+
+// cat.addEventListener("click", () => {
+//   if (catPic.className === "hide") {
+//     catPic.className = "";
+//   } else {
+//     catPic.className = "hide";
+//   }
+// });
+
+// const eagle = document.getElementById("eagle");
+// const eaglePic = document.getElementById("eaglePic");
+
+// eagle.addEventListener("click", () => {
+//   if (eaglePic.className === "hide") {
+//     eaglePic.className = "";
+//   } else {
+//     eaglePic.className = "hide";
+//   }
+// });

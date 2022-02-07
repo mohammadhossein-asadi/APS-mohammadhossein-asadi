5d408e8ccf05fa5ce66690b258d2dd9de1ed7836
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-02-07 18:23:48 +0330

    create and added files

---
+++ folder-16-2-7-2022/script.js
@@ -0,0 +1,87 @@
+const btn = document.querySelector("#v3");
+
+// console.log(btn);
+// console.dir(btn);
+// btn.onclick = () => {
+//   console.log("You haved clicked me!");
+// };
+
+//* What Does "(e)" Mean in JavaScript?
+//* Please pressed CTRL key
+// const btn = document.querySelector("#v3");
+
+// btn.addEventListener("click", function (e) {
+//   if (e.ctrlKey) {
+//     console.log("CTRL key was pressed while clicking.");
+//     console.log(e);
+//   }
+// });
+
+// const button = document.querySelector("#v3");
+
+// button.addEventListener("click", (event) => {
+//   button.textContent = `Click count: ${event.detail}`;
+// });
+
+//# key press
+
+// const log = document.getElementById("log");
+// const input = document.querySelector("input");
+
+// input.addEventListener("keypress", logKey);
+
+// function logKey(e) {
+//   log.textContent += ` ${e.code}`;
+// }
+
+// # another way ...
+// function scream() {
+//   console.log("Loud noise");
+// }
+
+// btn.onmouseenter = scream;
+
+// document.querySelector("h1").onclick = () => alert("You clicked the h1");
+
+// const heightOutput = document.querySelector("#height");
+// const widthOutput = document.querySelector("#width");
+
+// console.log(heightOutput, widthOutput);
+// console.log(window);
+// const reportWindowSize = () => {
+//   heightOutput.textContent = window.innerHeight;
+//   widthOutput.textContent = window.innerWidth;
+// };
+
+// window.addEventListener("resize", reportWindowSize);
+
+// btn.addEventListener("dblclick", () => alert("V3 clicked!"));
+
+// const twist = () => console.log("twits");
+// const shout = () => console.log("shout");
+
+// const tasButton = document.querySelector("#tas");
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+// tasButton.addEventListener("click", twist);
+// tasButton.addEventListener("click", shout);
+
+// # Quiz 1
+// const hello = document.querySelector("#hello");
+// hello.addEventListener("click", () => console.log("Hello ;)"));
+
+// const goodbye = document
+//   .querySelector("#goodbye")
+//   .addEventListener("click", () => console.log("Goodbye :("));
+
+// # Project
+
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
+const p = document.createElement("p");
+
+h1.append(p);
+
+

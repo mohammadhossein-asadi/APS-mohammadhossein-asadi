cbb4b4fa0fdddbba2e510f57e60f034675dd90c1
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-02-15 12:32:33 +0330

    change modified

--- folder-18-2-15-2022/script.js
+++ folder-18-2-15-2022/script.js
@@ -15,20 +15,50 @@
 //     console.log("This is in the catch " + message);
 //   });
 
-const recordVideoOne = new Promise((resolve, reject) => {
-  resolve("Video 1 Recorded");
-});
+// const recordVideoOne = new Promise((resolve, reject) => {
+//   resolve("Video 1 Recorded");
+// });
 
-const recordVideoTwo = new Promise((resolve, reject) => {
-  resolve("Video 2 Recorded");
-});
+// const recordVideoTwo = new Promise((resolve, reject) => {
+//   resolve("Video 2 Recorded");
+// });
 
-const recordVideoThree = new Promise((resolve, reject) => {
-  resolve("Video 3 Recorded");
-});
+// const recordVideoThree = new Promise((resolve, reject) => {
+//   resolve("Video 3 Recorded");
+// });
 
-Promise.all([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
-  (message) => {
-    console.log(message);
-  }
-);
+// Promise.all([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
+//   (message) => {
+//     console.log(message);
+//   }
+// );
+
+// const uno = () => {
+//   console.log("I am One");
+// };
+
+// const dos = () => {
+//   setTimeout(() => {
+//     console.log("Wooooooooo");
+//   }, 3000);
+//   console.log("I am Two");
+// };
+// const tres = () => {
+//   console.log("I am Three");
+// };
+// uno();
+// dos();
+// tres();
+
+// fetch("https://api.chucknorris.io/jokes/random")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     // console.log("DATA is: ", data);
+//     let joke = data.value;
+//     document.body.innerHTML = `JOKE: ${joke}`;
+//   })
+//   .catch((err) => console.log(err));
+
+

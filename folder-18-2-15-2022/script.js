498b0d6516e42f606004de22842761bc9f7214d4
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-02-15 11:56:59 +0330

    change

--- folder-18-2-15-2022/script.js
+++ folder-18-2-15-2022/script.js
@@ -15,4 +15,20 @@
 //     console.log("This is in the catch " + message);
 //   });
 
+const recordVideoOne = new Promise((resolve, reject) => {
+  resolve("Video 1 Recorded");
+});
 
+const recordVideoTwo = new Promise((resolve, reject) => {
+  resolve("Video 2 Recorded");
+});
+
+const recordVideoThree = new Promise((resolve, reject) => {
+  resolve("Video 3 Recorded");
+});
+
+Promise.all([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
+  (message) => {
+    console.log(message);
+  }
+);

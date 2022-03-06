50d27db71f69aa7b7b7c980aff0fdbb395d54b5b
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-06 12:21:56 +0330

    secound commit

--- folder-20-6-3-2022/script.js
+++ folder-20-6-3-2022/script.js
@@ -128,3 +128,105 @@ const fakeRequestPromise = (url) => {
 //   .then(() => rainbow("yellow", 1000))
 //   .then(() => rainbow("green", 1000))
 //   .catch((err) => console.log(err));
+
+// a regular function
+
+function greeting() {
+  console.log("Hello world!");
+}
+// greeting()
+
+const sing = async () => {
+  // return "la la la"
+  throw "oh no request error";
+  // throw new Error("there is a problem")
+};
+// console.log(sing());
+
+async function greet() {
+  console.log("Hellooooo");
+}
+// greet()
+
+// const login = async (username, password) => {
+//   if (!username || !password) {
+//     throw "Missing credential";
+//   }
+//   if (password === "jasem") {
+//     return `Welcome to www.mohammadhossein.com dear ${username}`;
+//   }
+
+//   throw "Invalid password";
+// };
+
+// login("mohammadhossein", "jasem")
+//   .then((msg) => {
+//     console.log("Logged in!");
+//     console.log(msg);
+//   })
+//   .catch((err) => console.log("Error", err));
+
+const delayedColorChange = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
+};
+
+async function rainbow2() {
+  await delayedColorChange("red", 1000);
+  delayedColorChange("orange", 1000);
+}
+// rainbow2();
+
+async function rainbow3() {
+  await delayedColorChange("red", 1000);
+  await delayedColorChange("green", 1000);
+  await delayedColorChange("yellow", 1000);
+  await delayedColorChange("gray", 1000);
+  await delayedColorChange("black", 1000);
+  await delayedColorChange("blue", 1000);
+  delayedColorChange("orange", 1000);
+}
+// rainbow3();
+
+async function printRainbow() {
+  await rainbow3();
+  console.log("End of rainbow");
+}
+// printRainbow()
+
+const fakeRequest = (url) => {
+  return new Promise((resolved, reject) => {
+    const delay = Math.random() * 4500 + 500;
+    setTimeout(() => {
+      if (delay > 1000) {
+        reject("connection timeout");
+      } else {
+        resolved(`here is your fake data: ${url}`);
+      }
+    });
+  });
+};
+
+async function makeTwoRequest() {
+  let data = await fakeRequest("test.com");
+  console.log(data);
+}
+// makeTwoRequest()
+
+async function makeTwoRequests() {
+  try {
+    let data1 = await fakeRequest("/page1");
+    console.log(data1);
+    let data2 = await fakeRequest("/page2");
+    console.log(data2);
+  } catch (err) {
+    console.log("caught an error!");
+    console.log("error is : ", err);
+  }
+}
+
+// makeTwoRequests()

23451ac8fcc4fbdd1acee95ca4a207df1870a234
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-07 12:15:25 +0330

    commit

---
+++ folder-21-7-3-2022/script.js
@@ -0,0 +1,112 @@
+const arr = [1, 2, 3];
+
+arr.sing = function (arg) {
+  return console.log(arg);
+};
+
+arr.sing(4);
+arr.push(4);
+
+// console.log(arr);
+
+const body = document.body;
+// console.dir(body);
+
+String.prototype.groot = (name) => alert(`hiii ${name}`);
+
+// const cat = "blueCat";
+
+// cat.groot("groot");
+
+// cat.toLowerCase();
+
+// const dog = "eco";
+
+// dog.groot("eco");
+
+// String.prototype.yell = function () {
+//   console.log(this.toUpperCase());
+// };
+// "Hello".yell();
+
+//# Factory function
+
+// const user1 = {
+//   name: "moahmmadhossein",
+//   email: "mohammadhossein@gmail.com",
+//   login() {
+//     return `The ${this.name} has just logged in`;
+//   },
+//   logout() {
+//     return `The ${this.name} has just logged out`;
+//   },
+// };
+
+// const user2 = {
+//   name: "moahmmad",
+//   email: "mohammad@gmail.com",
+//   login() {
+//     return `The ${this.name} has just logged in`;
+//   },
+//   logout() {
+//     return `The ${this.name} has just logged out`;
+//   },
+// };
+
+// function makeUser(username, email) {
+//   const user = {};
+//   user.name = username;
+//   user.email = email;
+//   user.info = function () {
+//     const { name, emailAddress } = this;
+//     return `user ${name} has this email: ${emailAddress}`;
+//   };
+
+//   user.login = function () {
+//     return `${user.name} has just logged in`;
+//   };
+
+//   user.email = function () {
+//     return `The user email is: ${this.emailAddress}`;
+//   };
+//   return user;
+// }
+
+// const person1 = makeUser("mohammad", "mohammad@gmail.com");
+// const person2 = makeUser("ali", "ali@gmail.com");
+// console.log(person1, person2);
+// console.log(person1.login());
+
+// function makeColor(r, g, b) {
+//   const color = {};
+//   color.r = r;
+//   color.g = g;
+//   color.b = b;
+//   color.rgb = function () {
+//     const { r, g, b } = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+//   };
+//   color.hex = function () {
+//     const { r, g, b } = this;
+//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   };
+//   return color;
+// }
+
+// console.log(makeColor(35, 255, 150).rgb());
+
+// const firstColor = makeColor(35, 255, 150);
+// console.log(firstColor);
+
+// const black = makeColor(0, 0, 0);
+
+// console.log(`${firstColor.hex()}\n, ${firstColor.rgb()}\n, ${black.hex()}`);
+
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+}
+
+const white = new Color(35, 0, 190);
+console.log(white);
